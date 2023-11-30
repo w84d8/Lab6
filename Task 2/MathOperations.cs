322eddaa89fc8953b78dd5cc012dadc48e2417c0
{{w84d8}} <{{rakonline99@gmail.com}}>
2023-11-30 13:03:36 +0200

    2

---
+++ Task 2/MathOperations.cs
@@ -0,0 +1,42 @@
+﻿class MathOperations
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int[] Add(int[] array1, int[] array2)
+    {
+        if (array1.Length != array2.Length)
+            throw new ArgumentException("Arrays must have the same length.");
+
+        int[] result = new int[array1.Length];
+        for (int i = 0; i < array1.Length; i++)
+        {
+            result[i] = array1[i] + array2[i];
+        }
+
+        return result;
+    }
+
+    public static int[,] Add(int[,] matrix1, int[,] matrix2)
+    {
+        int rows = matrix1.GetLength(0);
+        int columns = matrix1.GetLength(1);
+
+        if (rows != matrix2.GetLength(0) || columns != matrix2.GetLength(1))
+            throw new ArgumentException("Matrices must have the same dimensions.");
+
+        int[,] result = new int[rows, columns];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                result[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+
+        return result;
+    }
+
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+        int sum = MathOperations.Add(5, 10);
+        Console.WriteLine($"Sum of numbers: {sum}");
+
+        int[] array1 = { 1, 2, 3 };
+        int[] array2 = { 4, 5, 6 };
+        int[] arraySum = MathOperations.Add(array1, array2);
+        Console.WriteLine("Sum of arrays:");
+        Console.WriteLine(string.Join(", ", arraySum));
+
+        int[,] matrix1 = { { 1, 2 }, { 3, 4 } };
+        int[,] matrix2 = { { 5, 6 }, { 7, 8 } };
+        int[,] matrixSum = MathOperations.Add(matrix1, matrix2);
+        Console.WriteLine("Sum of matrices:");
+        for (int i = 0; i < matrixSum.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrixSum.GetLength(1); j++)
+            {
+                Console.Write($"{matrixSum[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
